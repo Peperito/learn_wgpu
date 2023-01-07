07c96e399a2e3bb493b8108e4bd9e76e9a7218b0
Vince P <32515747+Peperito@users.noreply.github.com>
2023-01-07 16:08:28 +0100

    fox

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,24 @@ struct Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+    Vertex { position: [0.0, 0.5, 0.0], color: [0.5, 0.0, 0.5] }, // 0
+    Vertex { position: [-0.5, 0.0, 0.0], color: [0.5, 0.0, 0.5] }, // 1
+    Vertex { position: [0.5, 0.0, 0.0], color: [0.5, 0.0, 0.5] }, // 2
+    Vertex { position: [0.0, -0.5, 0.0], color: [0.5, 0.0, 0.5] }, // 3
+    //Extras
+    Vertex { position: [-0.5, 0.5, 0.0], color: [0.5, 0.1, 0.3] }, // 4
+    Vertex { position: [0.5, 0.5, 0.0], color: [0.5, 0.1, 0.3] }, // 5
+    Vertex { position: [-0.25, 0.75, 0.0], color: [0.5, 0.1, 0.3] }, // 6
+    Vertex { position: [0.25, 0.75, 0.0], color: [0.5, 0.1, 0.3] }, // 7
 ];
 
 const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+    0,1,2,
+    1,3,2,
+    0,4,1,
+    5,0,2,
+    6,4,0,
+    7,0,5,
 ];
 
 struct State {
